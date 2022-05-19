65faabe0ce2cdfc180ef29fbfe3119a3e48b2e4b
israxelo <jejeisra@gmail.com>
2022-05-19 23:56:53 +0200

    con script de tablas

--- src/Conexion_y_Métodos/Conexion_y_metodos.java
+++ src/Conexion_y_Métodos/Conexion_y_metodos.java
@@ -3,6 +3,7 @@ package Conexion_y_Métodos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Conexion_y_metodos {
@@ -11,8 +12,8 @@ public class Conexion_y_metodos {
 	private static String password="proyecto";
 	private static String url="jdbc:oracle:thin:@localhost:1521/"+bd;
 	static Connection conexion =null;
-	static Statement state =null;
-	static ResultSet resul=null;
+	static Statement st =null;
+	static ResultSet rs =null;
 
 	public static void conectar() {
 		try {
@@ -25,6 +26,22 @@ public class Conexion_y_metodos {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public static void ver_todo() {
+		try {
+			st = conexion.createStatement();
+			rs = st.executeQuery("select * from PRUEBA");
+			while(rs.next()) {
+				System.out.println(rs.getString("COLUMNA1") + "   " + rs.getString("COLUMNA2"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	public static void main(String[] args) {
+		conectar();
+		ver_todo();
 	}
 	
 	
